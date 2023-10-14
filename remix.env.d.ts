8983bfd9706b6515b35a1acff0f2d9e5bd0e84ae
azu <azuciao@gmail.com>
2023-10-14 21:48:19 +0900

    update

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+// Need to load types before extending them
 import "@remix-run/server-runtime";
 declare module "@remix-run/server-runtime" {
     export interface AppLoadContext {
